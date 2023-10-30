5dba877eec1ae157c13c1192d210ddf475ef7500
Aquim <aquim422@gmail.com>
2023-10-30 18:04:57 -0300

    add item and sale

--- backend/domain/entities/item.py
+++ backend/domain/entities/item.py
@@ -6,9 +6,15 @@ class Item:
     id_: int
     name: str
     icon: str
+    year: int
+    color: str
+    manufacturer: str
     inserted_date: datetime
     approved: bool
     proposed_date: datetime
     annotation: str
+    purchase_price: float
+    sale_price: float
     sold: bool
     sale_date: datetime
+    sale_annotation: str
