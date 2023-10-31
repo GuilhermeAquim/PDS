212961f301d4e3def2fcc8d00c3f0bedfd6f8afe
augustomaillo <augusto.maillo@gmail.com>
2023-10-31 20:46:18 -0300

    Update in user_repository

--- backend/adapter/user_repository_sqlite.py
+++ backend/adapter/user_repository_sqlite.py
@@ -7,9 +7,12 @@ class UserRepositorySQLite(BaseSQLite, UserRepository):
     def __init__(self, database_path) -> None:
         super().__init__(database_path)
 
+    def _validate_admin(self, user_id):
+        user = self.get_user_by_id(user_id)
+        return user.admin
+    
     def create_user(self, session_user_id: int, login: str, password: str, admin: bool):
-        user = self.get_user_by_id(session_user_id)
-        if not user.admin:
+        if not self._validate_admin(session_user_id):
             raise PermissionError
         
         try:
@@ -23,6 +26,19 @@ class UserRepositorySQLite(BaseSQLite, UserRepository):
         args = (login, password, int(admin))
 
         return self.insert_data(query, args)
+    
+    def remove_user(self, session_user_id: int, to_remove_user_id: int):
+        if not self._validate_admin(session_user_id):
+            raise PermissionError
+        
+        try:
+            self.get_user_by_id(to_remove_user_id)
+        except UserNotFoundError as exc:
+            raise exc
+        
+        query = "DELETE FROM users WHERE id = ?"
+        args = (to_remove_user_id,)
+        self.delete_data(query, args)
 
     def get_user_by_id(self, user_id: int):
         query = "SELECT id, login, password, admin FROM users WHERE id = ?"
@@ -33,6 +49,8 @@ class UserRepositorySQLite(BaseSQLite, UserRepository):
         raise UserNotFoundError
 
     def get_user_by_login(self, login: str):
+        
+        
         query = "SELECT id, login, password, admin FROM users WHERE login = ?"
         args = (login, )
         row = self.select_data(query, args, fetchone=True)
@@ -40,8 +58,10 @@ class UserRepositorySQLite(BaseSQLite, UserRepository):
             return User(*row)
         raise UserNotFoundError
 
-
-    def set_admin_value(self, login:str, admin:bool):
+    def set_admin_value(self, session_user_id: int, new_admin_user_id: int, value : bool):
+        if not self._validate_admin(session_user_id):
+            raise PermissionError
+        
         query = "UPDATE users SET admin = ? WHERE login = ?"
-        args = (int(admin), login)
+        args = (int(value), new_admin_user_id)
         self.update_data(query, args)
