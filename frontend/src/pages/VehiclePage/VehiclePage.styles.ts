a69f14e69ba8ab692ac0f66ca1ff6d78ab363c58
Felipe Gonçalves <dpgoncalves.felipe@gmail.com>
2023-10-30 22:38:34 -0300

    adicionando projeto de frontend com página de login, página de listagem de estoque e página de venda de veículo

---
+++ frontend/src/pages/VehiclePage/VehiclePage.styles.ts
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+import ArrowBack from "@mui/icons-material/ArrowBack";
+
+export const VehiclePageHeader = styled("div")(() => ({
+  display: "flex",
+}));
+
+export const VehiclePageContent = styled("div")(() => ({
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr",
+  columnGap: 32,
+}));
+
+export const ArrowBackIcon = styled(ArrowBack)(() => ({
+  height: 40,
+  width: 40,
+  color: "#000",
+}));
---
+++ frontend/src/shared/components/VehicleCard/VehicleCard.tsx
@@ -0,0 +1,27 @@
+import { CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import { VehicleCardContainer } from "./VehicleCard.styles";
+import { useNavigate } from "react-router-dom";
+
+type Props = {
+  vehicle: any;
+};
+
+export const VehicleCard = ({ vehicle }: Props): JSX.Element => {
+  const navigate = useNavigate();
+
+  return (
+    <VehicleCardContainer onClick={() => navigate("/stock/1")}>
+      <CardHeader title="nome do carro" subheader="fabricante" />
+      <CardMedia
+        component="img"
+        sx={{ height: 164 }}
+        src="https://cdn.autopapo.com.br/box/uploads/2022/12/12165820/han-byd-carros-chineses-portal-732x488.jpg"
+      />
+      <CardContent>
+        <Typography>Vermelho</Typography>
+        <Typography>1998</Typography>
+        <Typography>168.456 km rodados, bom estado, sistema de som</Typography>
+      </CardContent>
+    </VehicleCardContainer>
+  );
+};
