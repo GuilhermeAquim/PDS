c949456014d4e6b45504e0beba4a07c9afa1a0d9
arthurbrito99 <arthur.brito1999@outlook.com>
2023-10-31 23:05:30 -0300

    fix sell items query

--- backend/adapter/sale_repository_sqlite.py
+++ backend/adapter/sale_repository_sqlite.py
@@ -24,10 +24,10 @@ class SaleRepositorySQLite(BaseSQLite, SaleRepository):
     
     def sell_item(self, item_id, sale_price, sale_annotation, sale_user_id) -> bool:
         
-        query = f""" SELECT id FROM items WHERE id = ? LIMIT 1"""
+        query = f""" SELECT id FROM items WHERE id = ? AND (sold is NULL OR sold != 1) AND approved = 1 LIMIT 1"""
         row = self.select_data(query,args=(item_id,), fetchone=True)
         if not row:
-            raise ItemNotExists(f'Sale {item_id} not exists.')
+            raise ItemNotExists(f'Item {item_id} not exists.')
         
         query = f""" SELECT id FROM users WHERE id = ? LIMIT 1"""
         row = self.select_data(query, args=(sale_user_id,), fetchone=True)
@@ -39,7 +39,8 @@ class SaleRepositorySQLite(BaseSQLite, SaleRepository):
                 sale_price = {sale_price},
                 sale_annotation = {"'" + sale_annotation + "'" if sale_annotation else 'NULL'},
                 sale_user_id = {sale_user_id},
-                sale_date = '{datetime.now()}'
+                sale_date = '{datetime.now()}',
+                sold = 1
             WHERE 
                 id = {item_id}
         """
